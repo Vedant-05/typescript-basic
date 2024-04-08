9f761a46f859773dfc068a983d24ec57194f20b0
Vedant Joshi <vedantj76@gmail.com>
2024-04-08 12:50:53 +0530

    Add files via upload

--- advance-Ts-Basic/src/index.ts
+++ advance-Ts-Basic/src/index.ts
@@ -13,6 +13,40 @@ type UserProfile = Pick<User, "name" | "email">;
 
 type UserProfileOptional = Partial<UserProfile>;
 
+type Event = "click" | "scroll" | "mousemove";
+type ExcludeEvent = Exclude<Event, "scroll">; // 'click' | 'mousemove'
+
+const handleEvent = (event: ExcludeEvent) => {
+  console.log(`Handling event: ${event}`);
+};
+
+handleEvent("click"); // OK
+
+interface User {
+  id: string;
+  name: string;
+}
+
+// Initialize an empty Map
+const usersMap = new Map<string, User>();
+
+// Add users to the map using .set
+usersMap.set("abc123", { id: "abc123", name: "John Doe" });
+usersMap.set("xyz789", { id: "xyz789", name: "Jane Doe" });
+
+interface Config {
+  readonly endpoint: string;
+  readonly apiKey: string;
+}
+
+const config: Readonly<Config> = {
+  endpoint: "https://api.example.com",
+  apiKey: "abcdef123456",
+};
+
+// Accessing a value using .get
+console.log(usersMap.get("abc123")); // Output: { id: 'abc123', name: 'John Doe' }
+
 const displayUserProfile = (user: UserProfileOptional) => {
   console.log(`Name: ${user.name}, Email: ${user.email}`);
 };
